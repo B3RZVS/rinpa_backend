17451fcd0f7e4e8406d6250373fa545c9e426208
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2025-07-08 10:15:10 -0300

    config bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    // otros módulos
+  ],
 })
 export class AppModule {}
